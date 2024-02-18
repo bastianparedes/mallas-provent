278e81388e7cdba44387cd946ecdb7c7a13801a5
Bastián <bparedesp@falabella.cl>
2024-02-18 14:33:23 -0300

    carrusel

--- src/app/components/Main/Carousel/index.tsx
+++ src/app/components/Main/Carousel/index.tsx
@@ -3,16 +3,22 @@ import Image from 'next/image';
 
 const Component = () => {
   return (
-    <section>
-      {pictures.map((picture, index) => (
-        <Image
-          alt={`Picture ${index}`}
-          className="object-contain w-full h-full"
-          key={index}
-          placeholder="blur"
-          src={picture}
-        />
-      ))}
+    <section className="px-2">
+      <div className="w-full h-52 overflow-hidden">
+        <div className="h-full flex">
+          {pictures.map((picture, index) => (
+            <div className="h-full aspect-square relative" key={index}>
+              <Image
+                alt={`Picture ${index}`}
+                className="object-cover w-full h-full"
+                key={index}
+                placeholder="blur"
+                src={picture}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
